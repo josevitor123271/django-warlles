55960c0ea652d266aa8fe731cec4273d030bb794
josevitor123271 <caang.2024119tads0019@aluno.ifpi.edu.br>
2025-10-22 16:59:51 -0300

    22/10/2025

--- setup/myapp/urls.py
+++ setup/myapp/urls.py
@@ -4,7 +4,21 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
+    # 1. Questão
     # path('', views.home, name='home'),
+    
+    # 2. Questão
     # Url personalizada com nome de usuário
-    path('saudacao/<str:nome>/', views.saudacao, name='saudacao'),
+    # path('saudacao/<str:nome>/', views.saudacao, name='saudacao'),
+
+    # 3. Questão
+    # path('soma/<int:num1>/<int:num2>/', views.soma, name='soma'),
+
+    # 4. Questão: Validação de data (Ano e mês)
+    # path('data/<int:ano>/<int:mes>/', views.validar_data, name='validar_data')
+
+    # 5. Questão: Rotas com parâmetros opcionais
+    path('perfil/', views.perfil, name='perfil'),
+    path('perfil/<str:username>/', views.perfil, name='perfil_nomeado')
+    # Exemplo: http://localhost:8000/perfil/leo/ (leo é o nome do usuário)
 ]
--- setup/myapp/views.py
+++ setup/myapp/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 
-# view simples de saudação
-def saudacao(request, nome):
-    return HttpResponse(f"Olá, {nome}! Seja bem vindo ao meu site!")
+# view simples de saudação (1. Questão)
+# def home(request):
+#     return HttpResponse("Olá, mundo! Seja bem vindo ao meu site!")
+
+# view simples de saudação (2. Questão)
+# def saudacao(request, nome):
+#     return HttpResponse(f"Olá, {nome}! Seja bem vindo ao meu site!")
+
+# View simples para soma de dois números (3. Questão)
+# def soma(request, num1, num2):
+#     return HttpResponse(f"A soma de {num1} e {num2} é {num1 + num2}")
+
+# View para validar data (Ano e mês) (4. Questão)
+# def validar_data(request, ano, mes):
+#     if 1 <= mes <= 12:
+#         return HttpResponse(f"A data {ano}-{mes} é válida.")
+#     else:
+#         return HttpResponse(f"A data {ano}-{mes} é inválida.")
+
+# View para rotas com parâmetros opcionais (5. Questão)
+# URL /perfil/ que, se receber um nome de uusário, mostre perfil do usuário, caso contrário, mostre o perfil de visitante.
+# Dica: usa kwargs ou uma lógica condicional
+def perfil(request, username=None):
+    if username:
+        return HttpResponse(f"Perfil do usuário: {username}")
+    else:
+        return HttpResponse("Perfil de visitante")
